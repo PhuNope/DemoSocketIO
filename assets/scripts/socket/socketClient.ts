15bd1d2f49d96cd04eeac8acec59e364ff302497
Nope <mrphu2001@gmail.com>
2023-01-15 22:43:49 +0700

    add socket

--- assets/scripts/socket/socketClient.ts
+++ assets/scripts/socket/socketClient.ts
@@ -5,80 +5,83 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import FbSdk from "../FbSDK/FbSdk";
-import GameController from "../gamecontroller";
+//import FbSdk from "../FbSDK/FbSdk";
+//import GameController from "../gamecontroller";
 
-const {ccclass, property} = cc._decorator;
+//const {ccclass, property} = cc._decorator;
 
-@ccclass
-export default class SocketClient extends cc.Component {
+// @ccclass
+// export default class SocketClient extends cc.Component {
 
-    @property(cc.Node)
-    mainGameNode:cc.Node;
-    mainGame;
+//     @property(cc.Node)
+//     mainGameNode:cc.Node;
+//     mainGame;
 
-    serverURL = 'https://fbmongochat.herokuapp.com/';
-    //serverURL = 'http://localhost:3000/';
-    // onLoad () {}
-    socket;
-    start(): void {
+//     serverURL = 'https://fbmongochat.herokuapp.com/';
+//     //serverURL = 'http://localhost:3000/';
+//     // onLoad () {}
+//     socket;
+//     start(): void {
 
-        setTimeout(() => {
-            this.mainGame = this.mainGameNode.getComponent(GameController)
-            this.onConnect();
-        }, 1000);
-    }
+//         setTimeout(() => {
+//             this.mainGame = this.mainGameNode.getComponent(GameController)
+//             this.onConnect();
+//         }, 1000);
+//     }
 
-    onConnect(){
-        console.log('On connected');
-        var self = this;
-        let play = (io) => {
-            this.socket = io.connect(this.serverURL);
-            //listen server emit
+//     onConnect(){
+//         console.log('On connected');
+//         var self = this;
+//         let play = (io) => {
+//             this.socket = io.connect(this.serverURL);
+//             //listen server emit
 
-            this.socket.on('connect_error', function(err) {
-                console.log('Connection failed',err);
-                self.socket.disconnect();
-            });
-            this.onListenFromServer();
-        }
-        //
-        if (typeof io === 'undefined') {
-            play(require('socket.io'));
-        }
-        else {
-            play(io);
-        }
-    }
-    private onListenFromServer(){
-        //server say hello
-        console.log('start connecting server');
-        //fetch all chat
-        this.socket.on('init_saved_message',(data)=>{
-            console.log('Server say:',data);
-            this.mainGame.initChat(data);
-            //this.sendToServer('chat message', 'mynamexxxx');
-            //-> reg name
-        })
-        this.socket.on('chat_connection',(data)=>{
-            console.log('Server say:',data);
-            //this.sendToServer('chat message', 'mynamexxxx');
-            //-> reg name
-        })
-        //recieve chat message
-        this.socket.on('user_chat_message',(data)=>{
-            console.log('someone say:',data);
-            this.mainGame.addChat(data);
+//             this.socket.on('connect_error', function(err) {
+//                 console.log('Connection failed',err);
+//                 self.socket.disconnect();
+//             });
+//             this.onListenFromServer();
+//         }
+//         //
+//         if (typeof io === 'undefined') {
+//             play(require('socket.io'));
+//         }
+//         else {
+//             play(io);
+//         }
+//     }
+//     private onListenFromServer(){
+//         //server say hello
+//         console.log('start connecting server');
+//         //fetch all chat
+//         this.socket.on('init_saved_message',(data)=>{
+//             console.log('Server say:',data);
+//             this.mainGame.initChat(data);
+//             //this.sendToServer('chat message', 'mynamexxxx');
+//             //-> reg name
+//         })
+//         this.socket.on('chat_connection',(data)=>{
+//             console.log('Server say:',data);
+//             //this.sendToServer('chat message', 'mynamexxxx');
+//             //-> reg name
+//         })
+//         //recieve chat message
+//         this.socket.on('user_chat_message',(data)=>{
+//             console.log('someone say:',data);
+//             this.mainGame.addChat(data);
 
-        })
-     
-    }
-    private sendToServer(connectionName: string,data: any){
-        this.socket.emit(connectionName, data);
-    }
-    public sendMessage(jsonMessage,callback){
-        this.socket.emit('user_chat_message', {sender:FbSdk.ins.fbPlayerName,locale:FbSdk.ins.fbLocale,photoURL:FbSdk.ins.fbPlayerPhotoURL,message:jsonMessage});
-        callback(true);
-    }
-    // update (dt) {}
-}
+//         })
+
+//     }
+//     private sendToServer(connectionName: string,data: any){
+//         this.socket.emit(connectionName, data);
+//     }
+//     public sendMessage(jsonMessage,callback){
+//         this.socket.emit('user_chat_message', {sender:FbSdk.ins.fbPlayerName,locale:FbSdk.ins.fbLocale,photoURL:FbSdk.ins.fbPlayerPhotoURL,message:jsonMessage});
+//         callback(true);
+//     }
+//     // update (dt) {}
+// }
+
+import { io } from "socket.io-client";
+
